039a9ccc4b61b194657ff8f6238d765fc578b6c3
Daniel Leibovitz <danleibovitz@gmail.com>
2020-11-30 16:21:25 +0100

    Move data generation for a partial linear model to data_generator.R

--- pseudo_data/data_generator.R
+++ pseudo_data/data_generator.R
@@ -1,3 +1,17 @@
-df1 <- gendata()
-df2 <- gendata()
-df3 <- gendata()
+X <- cbind(
+  runif(100, -5, 5),
+  runif(100, -10, 10),
+  runif(100, -100, 100),
+  runif(100, -100, 100),
+  runif(100, -100, 100)
+)
+
+W <- rep(1, 100)
+
+Y <- (X[,1])^3 + 1.5*X[,2] - 1.5*X[,3] - 2*X[,4] + X[,5] + rnorm(100, 0, 100)
+
+pseudo_df_1 <- data.frame(Y, X, W)
+
+
+# df2 <- gendata()
+# df3 <- gendata()
