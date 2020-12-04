00014e80a5bc91efe8acc75c2d72c7f1cd58225c
Daniel Leibovitz <danleibovitz@gmail.com>
2020-12-04 11:50:07 +0100

    mix up order of X matrix

--- pseudo_data/data_generator.R
+++ pseudo_data/data_generator.R
@@ -10,10 +10,14 @@ X <- cbind(
 W <- runif(100, 0, 1)
 W[c(10, 20, 30, 40, 50, 60, 70)] <- 0.000000e+00
 
-Y <- (X[,1])^3 + 1.5*X[,2] - 1.5*X[,3] - 2*X[,4] + X[,5] + rnorm(100, 0, 0.1)
+Y <- (X[,1])^5 + 1.5*X[,2] - 1.5*X[,3] - 2*X[,4] + X[,5] + rnorm(100, 0, 0.1)
 
 pseudo_df_1 <- data.frame(Y, X, W)
- 
+names(pseudo_df_1) <- c("Y", "X1", "X2", "X3", "X4", "X5", "W")
+hold <- pseudo_df_1$X1
+pseudo_df_1$X1 <- pseudo_df_1$X2
+pseudo_df_1$X2 <- hold
+
 
 # data with 4 latent categories
 X <- cbind(
@@ -35,6 +39,9 @@ Y4 <- 2*((X[751:1000,1])-5) - 3*X[751:1000,2] - 3*X[751:1000,3] - 3*X[751:1000,4
 
 pseudo_df_2 <- data.frame(c(Y1, Y2, Y3, Y4), X, W)
 names(pseudo_df_2) <- c("Y", "X1", "X2", "X3", "X4", "X5", "W")
+hold <- pseudo_df_2$X1
+pseudo_df_2$X1 <- pseudo_df_2$X2
+pseudo_df_2$X2 <- hold
 
 # df3 <- gendata()
 
